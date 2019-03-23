b1dafecf9ec95efe123bd20fe9479b64c4bd6bc7
donniezhangzq <donniezhangzq@gmail.com>
2019-03-23 12:09:38 +0800

    facai

--- gufu/facai/urls.py
+++ gufu/facai/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^version2$', views.version2, name='version2'),
 	url(r'^result$', views.result, name='result'),
+	url(r'^result_version2$', views.result_version2, name='result_version2'),
 	url(r'^getdata$', views.getdata, name='getdata'),
 ]
--- gufu/facai/views.py
+++ gufu/facai/views.py
@@ -11,6 +11,9 @@ from facai.models import Number
 def index(request):
 	return render(request, "index.html")
 
+def version2(request):
+	return render(request, "version2.html")
+
 def result(request):
 	number_1 = request.POST.get('number_1', '')
 	number_2 = request.POST.get('number_2', '')
@@ -41,45 +44,154 @@ def result(request):
 	data = {'rows': rows, "mapstr": mapstr}
 	return render(request, "result.html", data)
 
+def result_version2(request):
+	numbers = [0 for i in range(55)]
+	numbers_map = {}
+	i = 1
+	j = 0
+	k = 0
+	while i < 11:
+		j = i+1
+		while j < 12:
+			numbers[k] = "%d_%d" % (i, j)
+			k += 1
+			j += 1
+		i = i+1
+	
+	i = 0
+	try:
+		while i < 55:
+			key = "number_" + str(numbers[i])
+			numbers_map[numbers[i]] = int(request.POST.get(key, ""))
+			i += 1
+	except Exception:
+		return HttpResponse("some number is not interger")
+
+	rows = getdata_version2(numbers_map)
+	if len(rows) == 0:
+		return HttpResponse("something is error")
+
+	mapstr = ""
+	for i in numbers:
+		mapstr += "%s->%d | " % (i, numbers_map[i])
+
+	data = {'rows': rows, "mapstr": mapstr}
+
+	return render(request, "result_version2.html", data)
+
+
 def getdata(number_list):
 	rows = []
+	selist = get_setlist()
+
+	for s in setlist:
+		tmp = {}
+		tmp['set'] = ""
+		for i in s:
+			tmp['set'] += str(i) + " "
+
+		tmp['result'] = number_list[s[0]-1] + number_list[s[1]-1] + number_list[s[2]-1] + number_list[s[3]-1] + number_list[s[4]-1]
+		tmp['caculation'] = "%s%s%s%s%s" % (
+				tostr(number_list[s[0]-1], True),
+				tostr(number_list[s[1]-1], False),
+				tostr(number_list[s[2]-1], False),
+				tostr(number_list[s[3]-1], False),
+				tostr(number_list[s[4]-1], False))
+		rows.append(tmp)
+	return rows
+
+def getdata_version2(numbers_map):
+	rows = []
+	setlist = get_setlist()
+
+	for s in setlist:
+		tmp = {}
+		tmp['set'] = ""
+		for i in s:
+			tmp['set'] += str(i) + " "
+
+		c_list = c52_version3(s)
+		tmp['subset'] = " ".join(c_list)
+
+		if len(c_list) == 0:
+			return []
+
+		tmp['result'] = 0
+		tmp['caculation'] = ""
+		for i in c_list:
+			tmp['result'] += numbers_map[i]
+			if i == 0:
+				tmp['caculation'] += tostr(numbers_map[i], True)
+			else:
+				tmp['caculation'] += tostr(numbers_map[i], False)
+		rows.append(tmp)
+
+	return rows
+
+
+def c52(l):
+	result = [0 for i in range(10)]
+
+	if len(l) != 5:
+		return []
+	
+	l.sort()
+	i = 0
+	j = 0
+	t = 0
+	while i < 4:
+		j = i+1
+		while j < 5:
+			result[t] = "%d_%d" % (l[i], l[j])
+			t += 1
+			j += 1
+		i += 1
+	
+	if len(result) != 10:
+		return []
+
+	return result
+
+def c52_version3(l):
+	result = [0 for i in range(4)]
+	
+	if len(l) != 5:
+		return []
+
+	l.sort()
+	result[0] = "%d_%d" % (l[0], l[1])
+	result[1] = "%d_%d" % (l[0], l[2])
+	result[2] = "%d_%d" % (l[0], l[3])
+	result[3] = "%d_%d" % (l[0], l[4])
+
+	return result
+	
+
+def	get_setlist():
 	setlist = []
 	i=1
 	j=0
 	k=0
 	t=0
 	l=0
-	while i < 6:
+	while i < 8:
 		j = i+1
-		while j < 7:
+		while j < 9:
 			k= j+1
-			while k < 8:
+			while k < 10:
 				t = k+1
-				while t < 9:
+				while t < 11:
 					l= t+1
-					while l < 10:
-						setlist.append([str(i), str(j), str(k), str(t), str(l)])
+					while l < 12:
+						setlist.append([i, j, k, t, l])
 						l += 1
 					t += 1
 				k += 1
 			j += 1
 		i += 1
-	
-	for s in setlist:
-		tmp = {}
-		tmp['set'] = " ".join(s)
-		tmp['result'] = number_list[int(s[0])-1] + number_list[int(s[1])-1] + number_list[int(s[2])-1] + number_list[int(s[3])-1] + number_list[int(s[4])-1]
-		tmp['caculation'] = "%s%s%s%s%s" % (
-				tostr(int(s[0]), number_list, True),
-				tostr(int(s[1]), number_list, False),
-				tostr(int(s[2]), number_list, False),
-				tostr(int(s[3]), number_list, False),
-				tostr(int(s[4]), number_list, False))
-		rows.append(tmp)
-	return rows
+	return setlist
 
-def tostr(number, number_list, is_first):
-	tmp = number_list[int(number)-1] 
+def tostr(tmp, is_first):
 	if tmp >= 0:
 		if is_first:
 			result = str(tmp) 
